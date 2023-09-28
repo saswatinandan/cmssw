f8a8c36260a204c489a6b3a99c3a8f00f8ee6b69
Janice Chen <janice.chen45@gmail.com>
2023-09-28 17:46:31 +0200

    [new] for PbPb; [mod]

--- RecoLocalTracker/SiStripClusterizer/test/rootMacro/execute.sh
+++ RecoLocalTracker/SiStripClusterizer/test/rootMacro/execute.sh
@@ -1,9 +1,35 @@
 make sep19_4_matchAndPlot.o
-./sep19_4_matchAndPlot.o 34 > log/lumi_34.log
-./sep19_4_matchAndPlot.o 37 > log/lumi_37.log
-# ./sep19_4_matchAndPlot.o 40 > log/lumi_40.log
-./sep19_4_matchAndPlot.o 43 > log/lumi_43.log
-./sep19_4_matchAndPlot.o 46 > log/lumi_46.log
-./sep19_4_matchAndPlot.o 49 > log/lumi_49.log
-# ./sep19_4_matchAndPlot.o 52 > log/lumi_52.log
-./sep19_4_matchAndPlot.o 55 > log/lumi_55.log
+./sep19_4_matchAndPlot.o lumi_34 > log/lumi_34.log
+./sep19_4_matchAndPlot.o lumi_37 > log/lumi_37.log
+# ./sep19_4_matchAndPlot.o lumi_40 > log/lumi_40.log
+./sep19_4_matchAndPlot.o lumi_43 > log/lumi_43.log
+./sep19_4_matchAndPlot.o lumi_46 > log/lumi_46.log
+./sep19_4_matchAndPlot.o lumi_49 > log/lumi_49.log
+# ./sep19_4_matchAndPlot.o lumi_52 > log/lumi_52.log
+./sep19_4_matchAndPlot.o lumi_55 > log/lumi_55.log
+
+
+
+### sept 26th PbPb
+./sep19_4_matchAndPlot.o test_pbpb > log/test_pbpb.log
+./sep19_4_matchAndPlot.o test_pbpb_140 > log/test_pbpb_140.log
+
+./sep19_4_matchAndPlot.o online_rawprime_ls0051 > log/online_rawprime_ls0051.log
+./sep19_4_matchAndPlot.o offline_repacked_rawprime_ls0051 > log/offline_repacked_rawprime_ls0051.log
+
+
+### run 374288: some problem w/ ZS in different PD
+./sep19_4_matchAndPlot.o test_pbpb > log/test_pbpb.log
+### run 374322: [resolved] ~some problem w/ ZS in different PD~
+./sep19_4_matchAndPlot.o run374322_ls0025 > log/run374322_ls0025.log
+
+./sep19_4_matchAndPlot.o test_w_HLTmenu_on_VR > log/test_w_HLTmenu_on_VR.log
+
+./sep19_4_matchAndPlot.o RECO_NZS > log/RECO_NZS.log
+
+
+./sep19_4_matchAndPlot.o run374323_ls0258 > log/run374323_ls0258.log
+
+
+### new fill run 374345
+./sep19_4_matchAndPlot.o fixed_rawprime_run_374345_ls0029 > log/fixed_rawprime_run_374345_ls0029.log
