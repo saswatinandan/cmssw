84867abae0cf1a041948c2d5f6e32ebd279a0641
“saswatinandan” <saswati.nandan@cern.ch>
2024-09-02 13:11:57 +0200

    adding options for 2d hists

--- RecoLocalTracker/SiStripClusterizer/test/rootMacro/hist_auxiliary.h
+++ RecoLocalTracker/SiStripClusterizer/test/rootMacro/hist_auxiliary.h
@@ -3,12 +3,54 @@
 
 #include "TH1F.h"
 
+template<class T>
+void set_properties(T* h){
+
+	h->Sumw2();
+        h->SetDirectory(0);
+}
+
+auto createhist(const std::string& name, const std::string& title, const int& nbins, const float xlow=0.f, const float& xhigh=0.f){
+
+	TH1F* h = new TH1F(name.c_str(), title.c_str(), nbins, xlow, xhigh);
+        set_properties(h);
+        return h;
+}
+
+auto createhist(const std::string& name, const std::string& title, const int& nxbins, const float xlow, const float& xhigh, const int& nybins, const float ylow, const float& yhigh){
+
+         TH2F* h = new TH2F(name.c_str(), title.c_str(), nxbins, xlow, xhigh, nybins, ylow, yhigh);
+         set_properties(h);
+         return h;
+}
+
+//template<typename std::size_t S>
+auto createhist(const std::string& name, const std::string& title, const int& nbins, const double (&arry)[8]){
+
+         TH1F* h = new TH1F(name.c_str(), title.c_str(), nbins, arry);
+	 set_properties(h);
+         return h;
+}
+
+void Divide_w_sameDsets(TH1F* num, TH1F* denom, TH1F* ratio)
+{
+        for (int _x = 1; _x < num->GetNbinsX()+1; ++_x)
+        {
+                float _r        = denom->GetBinContent(_x) ? num->GetBinContent(_x)/denom->GetBinContent(_x) : 0.;
+                float _n_relErr = TMath::Abs(   num->GetBinError(_x)/  num->GetBinContent(_x) );
+                float _d_relErr = TMath::Abs( denom->GetBinError(_x)/denom->GetBinContent(_x) );
+                float _r_err    = TMath::Abs(_r) * ( (_n_relErr > _d_relErr) ? _n_relErr : _d_relErr  );
+                ratio->SetBinContent(_x, _r);
+                ratio->SetBinError(_x, _r_err);
+        }
+}
+
 template <typename T,
             typename = typename std::enable_if<std::is_arithmetic<T>::value>>
 T
 constrainValue(T value,
-                 T lowerBound,              
-                 T upperBound)
+               T lowerBound,              
+               T upperBound)
   {
     assert(lowerBound <= upperBound);
     value = std::max(value, lowerBound);    
@@ -18,7 +60,7 @@ constrainValue(T value,
 
 int fillWithOverFlow(TH1 * histogram,
                  double x,
-                 double evtWeight=1,
+                 double evtWeight=1.,
                  double evtWeightErr=0.)
 {
   if(!histogram) assert(0);
@@ -29,6 +71,24 @@ int fillWithOverFlow(TH1 * histogram,
   histogram->SetBinContent(bin, binContent + evtWeight);
 //  histogram->SetBinError(bin, std::sqrt(pow(binError,2) + 1));
   return ((bin == xAxis->GetNbins()) || (bin == 1)) ? 1 : 0;
+}
+
+int fillWithOverFlow(TH2 * histogram,
+                 double x,
+		 double y,
+                 double evtWeight=1.,
+                 double evtWeightErr=0.)
+{
+  if(!histogram) assert(0);
+  const TAxis * const xAxis = histogram->GetXaxis();
+  const TAxis * const yAxis = histogram->GetYaxis();
+  const int binx = constrainValue(xAxis->FindBin(x), 1, xAxis->GetNbins());
+  const int biny = constrainValue(yAxis->FindBin(y), 1, yAxis->GetNbins());
+  const double binContent = histogram->GetBinContent(binx, biny);
+  const double binError   = histogram->GetBinError(binx, biny);
+  histogram->SetBinContent(binx, biny, binContent + evtWeight);
+//  histogram->SetBinError(bin, std::sqrt(pow(binError,2) + 1));
+  return 0;//((bin == xAxis->GetNbins()) || (binx == 1)) ? 1 : 0;
 }
 
 template<class T>
