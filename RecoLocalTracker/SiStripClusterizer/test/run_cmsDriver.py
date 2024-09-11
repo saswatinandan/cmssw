ccb78f9a1b7b1449774301f4d923aa49ce143f55
Saswati <saswati.nandan@cern.ch>
2024-09-11 15:20:24 +0200

    adding code to run cmsDriver commands

---
+++ RecoLocalTracker/SiStripClusterizer/test/run_cmsDriver.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", dest="bit", default='8bit', help="bit to be studied")
+
+options = parser.parse_args()
+bit = options.bit
+
+def replace_line(infile, to_replaces, vals):
+
+    with open(infile, 'r') as f:
+      lines = f.readlines()
+
+    with open(infile, 'w') as f:
+        for line in lines:
+            for idx, to_replace in enumerate(to_replaces):
+               if to_replace in line:
+                  line = line.replace(to_replace, vals[idx])
+                  to_replaces.pop(idx)
+                  vals.pop(idx)
+                  break
+            f.write(line)
+
+### SiStripApproxCluster.h ####
+
+bit_no = int(bit.strip('bit')[0])
+maxRange_ = (1<<bit_no) -1
+replace_line('DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h',
+             ['maxRange_ = 255.'], [f'maxRange_ = {maxRange_}'])
+
+run_cmd = 'scram b -j 8'
+print(run_cmd)
+os.system(run_cmd)
+
+### hlt ###
+
+cmd_hlt = 'hltGetConfiguration /users/vmuralee/PREmenu/V9 --globaltag 140X_dataRun3_HLT_for2024TSGStudies_v1 --data --unprescale --max-events 100 --eras Run3 --input /store/data/Run2024F/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/382/216/00000/aadd1ab9-4eb8-4fb2-ac62-bdd1bebe882e.root > prehlt.py'
+
+#os.system(cmd_hlt)
+
+#replace_line('prehlt.py', ["'keep FEDRawDataCollection_rawPrimeDataRepacker_*_*',", "outputPhysicsHIPhysicsRawPrime0.root"],
+#              ["'keep *_*siStripClusters*_*_*',\n'keep *_*generalTracks*_*_*',\n'keep FEDRawDataCollection_raw*_*_HLTX',\n", f"outputPhysicsHIPhysicsRawPrime0_{bit}.root"])
+
+run_cmd = 'cmsRun prehlt.py'
+print(run_cmd)
+os.system(run_cmd)
+os.system(f'mv outputPhysicsHIPhysicsRawPrime0.root outputPhysicsHIPhysicsRawPrime0_{bit}.root')
+#### step2 ####
+
+cmd_step2 = "cmsDriver.py step2 --scenario pp --conditions auto:run3_data_prompt -s REPACK:DigiToApproxClusterRaw --datatier GEN-SIM-DIGI-RAW-HLTDEBUG --era Run3_pp_on_PbPb_approxSiStripClusters --eventcontent REPACKRAW -n 100 --customise_commands"
+cmd_step2 += ' "process.rawPrimeDataRepacker.src='
+cmd_step2 += "'rawDataRepacker'"
+cmd_step2 += f'" --repacked --process ReHLT --filein file:outputPhysicsHIPhysicsRawPrime0_{bit}.root --no_exec'
+
+print(cmd_step2)
+os.system(cmd_step2)
+replace_line('step2_REPACK.py', 
+              ['Run3_pp_on_PbPb_approxSiStripClusters', 'step2_REPACK.root'], ['Run3_pp_on_PbPb_approxSiStripClusters_2024', f'step2_REPACK_{bit}.root'])
+run_cmd = ('cmsRun step2_REPACK.py')
+print(run_cmd)
+os.system(run_cmd)
+
+###step3 ####
+
+cmd_step3 = f'cmsDriver.py step3 --conditions auto:run3_data_prompt -s RAW2DIGI,L1Reco,RECO --datatier RECO --eventcontent RECO --data --process reRECO --scenario pp -n 100 --repacked --era Run3_pp_on_PbPb_approxSiStripClusters --filein file:step2_REPACK_{bit}.root --no_exec --nThreads 4'
+
+print(cmd_step3)
+os.system(cmd_step3)
+
+replace_line('step3_RAW2DIGI_L1Reco_RECO.py',
+             ['Run3_pp_on_PbPb_approxSiStripClusters', 'step3_RAW2DIGI_L1Reco_RECO.root'], ['Run3_pp_on_PbPb_approxSiStripClusters_2024', f'step3_RAW2DIGI_L1Reco_RECO_{bit}.root'])
+run_cmd = 'cmsRun step3_RAW2DIGI_L1Reco_RECO.py'
+print(run_cmd)
+os.system(run_cmd)
+
+#### object comparison ####
+os.system(f'python3 run_flatNtuplizer.py -r step5_RAW2DIGI_L1Reco_RECO.root -rp step3_RAW2DIGI_L1Reco_RECO_{bit}.root -c')
