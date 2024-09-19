aa46a10ba820df22cfe7b229590725bb45d0f7f1
Saswati <saswati.nandan@cern.ch>
2024-09-19 21:57:33 +0200

    changing file name

--- RecoLocalTracker/SiStripClusterizer/test/run_allbit.py
+++ RecoLocalTracker/SiStripClusterizer/test/run_allbit.py
@@ -13,10 +13,14 @@ bits = options.bits
 with open('makefile', 'w') as f:
 
     allbit = ' '.join([str(b)+'bit' for b in bits])
-    f.write(f'all: {allbit}\n')
+    allbit_plot = allbit + ' plot'
+    f.write(f'all: {allbit_plot}\n')
     for bit in bits:
       f.write(f'{bit}bit:\n')
       f.write(f'\tbash build_env.sh {bit}bit {options.number} {options.threads} {options.remove}\n')
+    f.write(f'\nplot: {allbit}\n')
+    bits = ' '.join([b for b in bits])
+    f.write(f'\t python3 plot_comparison.py -b {bits}')
 
-os.system(f'make -f makefile -j {len(bits)}')
+#os.system(f'make -f makefile -j {len(bits)}')
  
