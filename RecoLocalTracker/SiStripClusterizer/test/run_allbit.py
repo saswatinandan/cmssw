1fb1795620e374adfa261a91410b139b7a44e972
Saswati <saswati.nandan@cern.ch>
2024-09-30 10:49:28 +0200

    add options to run all bits without running the cmsDriver commands

--- RecoLocalTracker/SiStripClusterizer/test/run_allbit.py
+++ RecoLocalTracker/SiStripClusterizer/test/run_allbit.py
@@ -6,6 +6,7 @@ parser.add_argument("-n", dest="number", default='100', help="how many numbers o
 parser.add_argument("-b", dest="bits", nargs='+', default=[16, 10, 12, 14, 8, 6, 4], help="bit to be studied")
 parser.add_argument("-t", dest="threads", default=20, help="# of threads")
 parser.add_argument("-r", type=int, dest="remove", default=0, help="want to delete directory")
+parser.add_argument("-c", action='store_false', dest="cms_command", default=1, help="either want to run cmsDriver commands or only the .cc file")
 
 options = parser.parse_args()
 bits = options.bits
@@ -17,7 +18,10 @@ with open('makefile', 'w') as f:
     f.write(f'all: {allbit_plot}\n')
     for bit in bits:
       f.write(f'{bit}bit:\n')
-      f.write(f'\tbash build_env.sh {bit}bit {options.number} {options.threads} {options.remove}\n')
+      if options.cms_command:
+         f.write(f'\tbash build_env.sh {bit}bit {options.number} {options.threads} {options.remove}\n')
+      else:
+         f.write(f'\tcd output_{bit}bit && ../rootMacro/LHCC_rawprime_clusters.o flatntuple_step3_RAW2DIGI_L1Reco_RECO_{bit}bit.root ~/backup/flatntuple_step5_RAW2DIGI_L1Reco_RECO.root > cluster.log && ../rootMacro/LHCC_raw_vs_rawprime.o flatntuple_step3_RAW2DIGI_L1Reco_RECO_{bit}bit.root ~/backup/flatntuple_step5_RAW2DIGI_L1Reco_RECO.root > object.log\n')
     f.write(f'\nplot: {allbit}\n')
     bits = ' '.join([b for b in bits])
     f.write(f'\t python3 plot_comparison.py -b {bits}')
