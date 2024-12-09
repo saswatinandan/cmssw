445d8a48f065e267faa2fdabcbb380f0a8b6a092
Saswati <saswati.nandan@cern.ch>
2024-12-09 11:27:53 +0100

    add new file to cross check the file size vs bit

--- RecoLocalTracker/SiStripClusterizer/test/prehlt.py
+++ RecoLocalTracker/SiStripClusterizer/test/prehlt.py
@@ -536,7 +536,8 @@ process.schedule = cms.Schedule( *(process.HLT_HIL1NotBptxOR_v8, process.Dataset
 # source module (EDM inputs)
 process.source = cms.Source( "PoolSource",
     fileNames = cms.untracked.vstring(
-        '/store/data/Run2024F/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/382/216/00000/aadd1ab9-4eb8-4fb2-ac62-bdd1bebe882e.root',
+        #'/store/data/Run2024F/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/382/216/00000/aadd1ab9-4eb8-4fb2-ac62-bdd1bebe882e.root',
+        '/store/data/Run2024F/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/382/229/00000/0fd42df0-c533-49df-ae69-8a7195b340c3.root',
     ),
     inputCommands = cms.untracked.vstring(
         'keep *'
@@ -588,7 +589,7 @@ _customInfo['inputFiles'][True]  = "file:RelVal_Raw_GRun_DATA.root"
 _customInfo['inputFiles'][False] = "file:RelVal_Raw_GRun_MC.root"
 _customInfo['maxEvents' ]=  -1
 _customInfo['globalTag' ]= "140X_dataRun3_Prompt_v3"
-_customInfo['inputFile' ]=  ['/store/data/Run2024F/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/382/216/00000/aadd1ab9-4eb8-4fb2-ac62-bdd1bebe882e.root']
+_customInfo['inputFile' ]=  ['/store/data/Run2024F/Muon0/RAW-RECO/ZMu-PromptReco-v1/000/382/229/00000/0fd42df0-c533-49df-ae69-8a7195b340c3.root']
 _customInfo['realData'  ]=  True
 
 from HLTrigger.Configuration.customizeHLTforALL import customizeHLTforAll
