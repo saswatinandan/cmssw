b784cf6b5310e26ac102c3e05537fab486cac982
Saswati <saswati.nandan@cern.ch>
2025-03-03 20:37:50 +0100

    add changes to keep detid diff and beginindicess diff

--- RecoLocalTracker/SiStripClusterizer/plugins/SiStripApprox2Clusters.cc
+++ RecoLocalTracker/SiStripClusterizer/plugins/SiStripApprox2Clusters.cc
@@ -1,6 +1,6 @@
 #include "DataFormats/Common/interface/DetSetVectorNew.h"
 #include "DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h"
-#include "DataFormats/SiStripCluster/interface/SiStripApproximateClusterCollection.h"
+#include "DataFormats/SiStripCluster/interface/SiStripApproximateClusterCollection_v1.h"
 #include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -25,7 +25,7 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
 
 private:
-  edm::EDGetTokenT<SiStripApproximateClusterCollection> clusterToken_;
+  edm::EDGetTokenT<v1::SiStripApproximateClusterCollection> clusterToken_;
   edm::ESGetToken<TrackerGeometry, TrackerDigiGeometryRecord> tkGeomToken_;
 };
 
@@ -42,19 +42,39 @@ void SiStripApprox2Clusters::produce(edm::StreamID id, edm::Event& event, const 
   const auto& tkGeom = &iSetup.getData(tkGeomToken_);
   const auto& tkDets = tkGeom->dets();
 
+  std::vector<uint16_t> v_strip;
+  float previous_barycenter = -999.;
+  unsigned int module_length = 0;
+  unsigned int previous_module_length = 0;
+
+  //std::cout << "event " << event.id().event() << "\t" <<  event.id().run() << "\t" << event.id().luminosityBlock() << std::endl;
+  unsigned int clusBegin = 0;
   for (const auto& detClusters : clusterCollection) {
+    //if (event.id().event() != 8168080) continue;
     edmNew::DetSetVector<SiStripCluster>::FastFiller ff{*result, detClusters.id()};
     unsigned int detId = detClusters.id();
-
+    //std::cout << "detId " << detId << std::endl;
     uint16_t nStrips{0};
     auto det = std::find_if(tkDets.begin(), tkDets.end(), [detId](auto& elem) -> bool {
       return (elem->geographicalId().rawId() == detId);
     });
     const StripTopology& p = dynamic_cast<const StripGeomDetUnit*>(*det)->specificTopology();
-    nStrips = p.nstrips() - 1;
-
+    nStrips = p.nstrips();
+    v_strip.push_back(nStrips);
+    previous_module_length += (v_strip.size() <3) ? 0 : v_strip[v_strip.size()-3];
+    module_length += (v_strip.size() <2) ? 0 : v_strip[v_strip.size()-2];
+    bool first_cluster = true;
+    detClusters.move(clusBegin);
+    //std::cout << "detId " << detId << std::endl;
     for (const auto& cluster : detClusters) {
-      ff.push_back(SiStripCluster(cluster, nStrips));
+      const auto convertedCluster = SiStripCluster(cluster, nStrips-1, previous_barycenter, module_length, first_cluster ? previous_module_length : module_length);
+      if ( (convertedCluster.barycenter()) >= nStrips) {
+         break;
+      }
+      previous_barycenter = convertedCluster.barycenter();
+      ++clusBegin;
+      ff.push_back(convertedCluster);
+      first_cluster = false; 
     }
   }
 
