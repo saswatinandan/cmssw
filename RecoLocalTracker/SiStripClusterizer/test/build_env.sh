1afc60f16688e17c9081e00570b328399e19d3b1
Saswati <saswati.nandan@cern.ch>
2025-03-04 19:09:21 +0100

    correction

--- RecoLocalTracker/SiStripClusterizer/test/build_env.sh
+++ RecoLocalTracker/SiStripClusterizer/test/build_env.sh
@@ -9,7 +9,7 @@ export output=/scratch/$(whoami)/$7
 export tmp=/home/users/$(whoami)/tmp/$7
 export cluster=$8
 export git_branch=$9
-export strip_charge_cut=$10
+export strip_charge_cut=${10}
 
 echo $tmp
 echo $output
@@ -43,6 +43,8 @@ eval "git branch -v >>  git.log"
 export current_dir=$(pwd)
 echo 'curent dir: ', $current_dir
 
+echo $strip_charge_cut
+echo "python3 run_cmsDriver.py -b $barycenter_bit -w $width_bit -a $avgCharge_bit -n $evn -t $thread -c $cluster -s $strip_charge_cut"
 python3 run_cmsDriver.py -b $barycenter_bit -w $width_bit -a $avgCharge_bit -n $evn -t $thread -c $cluster -s $strip_charge_cut
 
 eval "cd ${output}"
