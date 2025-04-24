d2869a15ab171803ad3f7b6d4e0a0f32284c0af8
Saswati <saswati.nandan@cern.ch>
2025-04-24 20:59:11 +0200

    plotting style

--- RecoLocalTracker/SiStripClusterizer/test/plot_comparison_singleplot.py
+++ RecoLocalTracker/SiStripClusterizer/test/plot_comparison_singleplot.py
@@ -167,28 +167,28 @@ else:
   sizes = {}
   yvals = {}
 
-  texts["chargecut_raw'"] = []
-  yvals["chargecut_raw'"] = []
-  sizes["chargecut_raw'"] = []
+  texts["raw':chargecut"] = []
+  yvals["raw':chargecut"] = []
+  sizes["raw':chargecut"] = []
 
-  update_list('HI_wchargecut_saswati', 15, 6, "rawp", sizes, yvals, texts, "chargecut_raw'", options.events)
+  update_list('HI_wchargecut_saswati', 15, 6, "rawp", sizes, yvals, texts, "raw':chargecut", options.events)
 
-  texts["no_chargecut_raw'"] = []
-  yvals["no_chargecut_raw'"] = []
-  sizes["no_chargecut_raw'"] = []
+  texts["raw':no chargecut"] = []
+  yvals["raw':no chargecut"] = []
+  sizes["raw':no chargecut"] = []
 
-  update_list('HI_wochargecut_saswati', 15, 6, "rawp", sizes, yvals, texts, "no_chargecut_raw'", options.events)
+  update_list('HI_wochargecut_saswati', 15, 6, "rawp", sizes, yvals, texts, "raw':no chargecut", options.events)
 
-  texts["chargecut_HI_raw'"] = []
-  yvals["chargecut_HI_raw'"] = []
-  sizes["chargecut_HI_raw'"] = []
+  texts["HI_raw':chargecut"] = []
+  yvals["HI_raw':chargecut"] = []
+  sizes["HI_raw':chargecut"] = []
 
-  update_list('HI_wchargecut', 16, 8, "rawp", sizes, yvals, texts, "chargecut_HI_raw'", options.events)
+  update_list('HI_wchargecut', 16, 8, "rawp", sizes, yvals, texts, "HI_raw':chargecut", options.events)
 
-  texts["no_chargecut_HI_raw'"] = []
-  yvals["no_chargecut_HI_raw'"] = []
-  sizes["no_chargecut_HI_raw'"] = []
+  texts["HI_raw':no chargecut"] = []
+  yvals["HI_raw':no chargecut"] = []
+  sizes["HI_raw':no chargecut"] = []
 
-  update_list('HI_wochargecut', 16, 8, 'rawp', sizes, yvals, texts, "no_chargecut_HI_raw'", options.events)
+  update_list('HI_wochargecut', 16, 8, 'rawp', sizes, yvals, texts, "HI_raw':no chargecut", options.events)
 
   draw_trackno(sizes, yvals, texts, '# of tracks', 'tracks', 'rawp')
