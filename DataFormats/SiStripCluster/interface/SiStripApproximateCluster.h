dbe49de8f2034eeeabbcd025b1829629535ddb33
“saswatinandan” <saswati.nandan@cern.ch>
2025-05-20 21:28:28 +0200

    implementing v2

--- DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
+++ DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
@@ -2,21 +2,23 @@
 #define DataFormats_SiStripCluster_SiStripApproximateCluster_h
 
 #include "FWCore/Utilities/interface/typedefs.h"
+#include "assert.h"
+#include <iostream>
 
 class SiStripCluster;
 class SiStripApproximateCluster {
 public:
   SiStripApproximateCluster() {}
 
-  explicit SiStripApproximateCluster(cms_uint16_t barycenter,
+  explicit SiStripApproximateCluster(cms_uint16_t compBarycenter,
                                      cms_uint8_t width,
-                                     cms_uint8_t avgCharge,
+                                     cms_uint8_t compavgCharge,
                                      bool filter,
                                      bool isSaturated,
                                      bool peakFilter = false)
-      : barycenter_(barycenter),
+      : compBarycenter_(compBarycenter),
         width_(width),
-        avgCharge_(avgCharge),
+        compavgCharge_(compavgCharge),
         filter_(filter),
         isSaturated_(isSaturated),
         peakFilter_(peakFilter) {}
@@ -24,22 +26,46 @@ public:
   explicit SiStripApproximateCluster(const SiStripCluster& cluster,
                                      unsigned int maxNSat,
                                      float hitPredPos,
+                                     float& previous_cluster,
+                                     unsigned int& module_length,
+                                     unsigned int& previous_module_length,
                                      bool peakFilter);
 
-  cms_uint16_t barycenter() const { return barycenter_; }
-  cms_uint8_t width() const { return width_; }
-  cms_uint8_t avgCharge() const { return avgCharge_; }
+  const cms_uint16_t compBarycenter() const {
+    return compBarycenter_;
+  }
+
+  float barycenter(float previous_barycenter=0,
+                   unsigned int module_length=0, unsigned int previous_module_length=0) const {
+    float _barycenter;
+    if (  previous_barycenter == -999 )
+      _barycenter = compBarycenter_ * maxBarycenter_/maxRange_;
+    else {
+      _barycenter = ((compBarycenter_ * maxBarycenter_/maxRange_) - (module_length-previous_module_length)) + previous_barycenter;
+    }
+  //  std::cout << ".h compBarycenter_= " << compBarycenter_ << ", barycenter= " << _barycenter << ", module length= " << module_length  << ", previous barycenter= " << previous_barycenter << ", previous_module_length= " << previous_module_length << std::endl;
+    assert(_barycenter <= maxBarycenter_ && "Returning barycenter > maxBarycenter");
+    return _barycenter; }
+  cms_uint8_t width() const {return width_; }
+  float avgCharge() const { 
+     float avgCharge_ = compavgCharge_ * maxavgCharge_/maxavgChargeRange_ ;
+    assert(avgCharge_ <= maxavgCharge_ && "Returning avgCharge > maxavgCharge");
+     return avgCharge_; }
   bool filter() const { return filter_; }
   bool isSaturated() const { return isSaturated_; }
   bool peakFilter() const { return peakFilter_; }
 
 private:
-  cms_uint16_t barycenter_ = 0;
+  cms_uint16_t compBarycenter_ = 0;
   cms_uint8_t width_ = 0;
-  cms_uint8_t avgCharge_ = 0;
+  cms_uint8_t compavgCharge_ = 0;
   bool filter_ = false;
   bool isSaturated_ = false;
   bool peakFilter_ = false;
+  static constexpr double maxRange_ = 32767;
+  static constexpr double maxBarycenter_ = 1536.;
+  static constexpr double maxavgChargeRange_ = 63; //255; //255; //255;
+  static constexpr double maxavgCharge_ = 255.;
   static constexpr double trimMaxADC_ = 30.;
   static constexpr double trimMaxFracTotal_ = .15;
   static constexpr double trimMaxFracNeigh_ = .25;
--- DataFormats/SiStripCluster/src/SiStripApproximateCluster.cc
+++ DataFormats/SiStripCluster/src/SiStripApproximateCluster.cc
@@ -2,14 +2,29 @@
 #include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
 #include <algorithm>
 #include <cmath>
+#include <assert.h>
 
 SiStripApproximateCluster::SiStripApproximateCluster(const SiStripCluster& cluster,
                                                      unsigned int maxNSat,
                                                      float hitPredPos,
+                                                     float& previous_cluster,
+                                                     unsigned int& module_length,
+                                                     unsigned int& previous_module_length,
                                                      bool peakFilter) {
-  barycenter_ = std::round(cluster.barycenter() * 10);
-  width_ = cluster.size();
-  avgCharge_ = cluster.charge() / cluster.size();
+  if (previous_cluster == -999.)
+   compBarycenter_ = std::round(cluster.barycenter() * maxRange_/maxBarycenter_);
+  else
+   compBarycenter_ = std::round(((cluster.barycenter()-previous_cluster)+(module_length-previous_module_length))* maxRange_/maxBarycenter_);// + module_length;
+  //std::cout << ".cc compBarycenter_= " << compBarycenter_ << ", barycenter= " << cluster.barycenter() <<  ", module length= " << module_length  << ", previous barycenter= " << previous_cluster << ", previous_module_length= " << previous_module_length << std::endl;
+  //std::cout << ((cluster.barycenter()+module_length)-(previous_cluster+previous_module_length)) << std::endl;
+  previous_cluster = barycenter(previous_cluster, module_length, previous_module_length);
+  assert(cluster.barycenter() <= maxBarycenter_ && "Got a barycenter > maxBarycenter");
+  assert(compBarycenter_ <= maxRange_ && "Filling compBarycenter > maxRange");
+  width_ = std::min(255,(int)cluster.size());
+  float avgCharge_ = cluster.charge() / width_;
+  assert(avgCharge_ <= maxavgCharge_ && "Got a avgCharge > maxavgCharge");
+  compavgCharge_ = std::round(avgCharge_ * maxavgChargeRange_/maxavgCharge_);
+  assert(compavgCharge_ <= maxavgChargeRange_ && "Filling compavgCharge > maxavgChargeRange");
   filter_ = false;
   isSaturated_ = false;
   peakFilter_ = peakFilter;
