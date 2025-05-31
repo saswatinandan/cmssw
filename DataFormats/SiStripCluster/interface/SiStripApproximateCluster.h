d8c135f785f9ff7eb66c40f33b5ee2b7c4815075
Saswati <saswati.nandan@cern.ch>
2025-05-31 15:40:18 +0200

    splitting method test

--- DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
+++ DataFormats/SiStripCluster/interface/SiStripApproximateCluster.h
@@ -17,8 +17,8 @@ public:
                                      bool isSaturated,
                                      bool peakFilter = false)
       : compBarycenter_(compBarycenter),
-        width_(width),
-        compavgCharge_(compavgCharge),
+        //width_(width),
+        //compavgCharge_(compavgCharge),
         filter_(filter),
         isSaturated_(isSaturated),
         peakFilter_(peakFilter) {}
@@ -36,34 +36,34 @@ public:
 
   float barycenter(float previous_barycenter=0,
                    unsigned int module_length=0, unsigned int previous_module_length=0) const {
-    float _barycenter;
-    if (  previous_barycenter == -999 )
-      _barycenter = compBarycenter_ * maxBarycenter_/maxRange_;
-    else {
-      _barycenter = ((compBarycenter_ * maxBarycenter_/maxRange_) - (module_length-previous_module_length)) + previous_barycenter;
-    }
+    //float _barycenter;
+    //if (  previous_barycenter == -999 )
+      //_barycenter = compBarycenter_ * maxBarycenter_/maxRange_;
+    //else {
+      //_barycenter = ((compBarycenter_ * maxBarycenter_/maxRange_) - (module_length-previous_module_length)) + previous_barycenter;
+    //}
   //  std::cout << ".h compBarycenter_= " << compBarycenter_ << ", barycenter= " << _barycenter << ", module length= " << module_length  << ", previous barycenter= " << previous_barycenter << ", previous_module_length= " << previous_module_length << std::endl;
-    assert(_barycenter <= maxBarycenter_ && "Returning barycenter > maxBarycenter");
-    return _barycenter; }
-  cms_uint8_t width() const {return width_; }
+    //assert(_barycenter <= maxBarycenter_ && "Returning barycenter > maxBarycenter");
+    return 0;}//_barycenter; }
+  cms_uint8_t width() const {return 0;}//width_; }
   float avgCharge() const { 
-     float avgCharge_ = compavgCharge_ * maxavgCharge_/maxavgChargeRange_ ;
-    assert(avgCharge_ <= maxavgCharge_ && "Returning avgCharge > maxavgCharge");
-     return avgCharge_; }
+     //float avgCharge_ = compavgCharge_ * maxavgCharge_/maxavgChargeRange_ ;
+    //assert(avgCharge_ <= maxavgCharge_ && "Returning avgCharge > maxavgCharge");
+     return 0;}//avgCharge_; }
   bool filter() const { return filter_; }
   bool isSaturated() const { return isSaturated_; }
   bool peakFilter() const { return peakFilter_; }
-
+  uint8_t all=0;
 private:
   cms_uint16_t compBarycenter_ = 0;
-  cms_uint8_t width_ = 0;
-  cms_uint8_t compavgCharge_ = 0;
+  //cms_uint8_t width_ = 0;
+  //cms_uint8_t compavgCharge_ = 0;
   bool filter_ = false;
   bool isSaturated_ = false;
   bool peakFilter_ = false;
   static constexpr double maxRange_ = 32767; //16383; //16383; //32767;
   static constexpr double maxBarycenter_ = 1536.;
-  static constexpr double maxavgChargeRange_ = 255; //255; //255; //255;
+  static constexpr double maxavgChargeRange_ = 63; //255; //255; //255;
   static constexpr double maxavgCharge_ = 255.;
   static constexpr double trimMaxADC_ = 30.;
   static constexpr double trimMaxFracTotal_ = .15;
--- DataFormats/SiStripCluster/src/SiStripApproximateCluster.cc
+++ DataFormats/SiStripCluster/src/SiStripApproximateCluster.cc
@@ -3,7 +3,44 @@
 #include <algorithm>
 #include <cmath>
 #include <assert.h>
+#include <bitset>
+uint8_t reverseByte(uint8_t b) {
+   uint8_t rev = 0;
+   for (int i = 0; i < 8; ++i) {
+	rev <<= 1;
+	rev |= (b & 1);
+        b >>= 1;
+    }
+   return rev;
+}
+void printBits_16(uint16_t value) {
+   //Method 1: Using std::bitset (easy and clean)
+   std::bitset<16> bits(value);
+   std::cout << bits << std::endl;
+}
+void printBits_8(uint8_t value) {
+    // Method 1: Using std::bitset (easy and clean)
+    std::bitset<8> bits(value);
+    std::cout << bits << std::endl;}
+void printBits_32(uint32_t value) {
+    // Method 1: Using std::bitset (easy and clean)
+    std::bitset<32> bits(value);
+    std::cout << bits << std::endl;}
+int bits_used(uint8_t x) {
+	for (int i = 7; i >= 0; --i) {
+          //std::cout << i << "\t" << (int)x << "\t" << (x & (1 << i)) << std::endl;
+	  if (x & (1 << i)) return i + 1;
+	}
+	return 0;
+}
 
+uint16_t compress(uint8_t a, uint8_t b) {
+	int bits_a = bits_used(a);
+	int bits_b = bits_used(b);
+	// Shift b to make room for a in the lower bits
+	uint16_t packed = ((uint16_t)b << bits_a) | a;
+	return packed;
+}
 SiStripApproximateCluster::SiStripApproximateCluster(const SiStripCluster& cluster,
                                                      unsigned int maxNSat,
                                                      float hitPredPos,
@@ -11,6 +48,9 @@ SiStripApproximateCluster::SiStripApproximateCluster(const SiStripCluster& clust
                                                      unsigned int& module_length,
                                                      unsigned int& previous_module_length,
                                                      bool peakFilter) {
+  //cms_uint16_t compBarycenter_=0;
+  cms_uint8_t width_ = 0;
+  cms_uint8_t compavgCharge_ = 0;
   if (previous_cluster == -999.)
    compBarycenter_ = std::round(cluster.barycenter() * maxRange_/maxBarycenter_);
   else
@@ -28,7 +68,29 @@ SiStripApproximateCluster::SiStripApproximateCluster(const SiStripCluster& clust
   filter_ = false;
   isSaturated_ = false;
   peakFilter_ = peakFilter;
-
+  uint8_t lsb = compBarycenter_ & 0xFF;
+  uint8_t hsb = (compBarycenter_ >> 8) & 0xFF;
+  all = compress(width_,compavgCharge_);
+  //all |= ((uint32_t)lsb) <<24;
+  uint8_t rev_ = reverseByte(compavgCharge_);
+  //all |= ((uint16_t)rev_) << 8;
+  //all |= width_;
+  //uint8_t rev_ = reverseByte(width_);
+  //all |= ((uint32_t)rev_)<<16;
+  //all |= ((uint32_t)lsb)<<16;
+  //all |= ((uint32_t)rev_)<<8;
+  //all |= ((uint32_t)hsb)<<8;
+  uint8_t revc_ = reverseByte(compavgCharge_);
+  //all |=compavgCharge_;
+  //all |= hsb;
+  //std::cout << "bary: " << compBarycenter_ << std::endl;
+  //printBits_16(compBarycenter_);
+  //std::cout << "width: " << (int)width_ << std::endl;
+  //printBits_8(width_);
+  //std::cout << "charge: " << (int)compavgCharge_ << std::endl;
+  //printBits_8(compavgCharge_);
+  //std::cout << "all " << std::endl;
+  //printBits_16(all);
   //mimicing the algorithm used in StripSubClusterShapeTrajectoryFilter...
   //Looks for 3 adjacent saturated strips (ADC>=254)
   const auto& ampls = cluster.amplitudes();
